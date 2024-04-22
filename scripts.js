c3eab132612f85f9936f00c29c56a25c87efa353
bNegro <72948318+BNegro1@users.noreply.github.com>
2024-04-22 13:43:07 -0400

    Actualización .html y .js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const l_notasPor = [];
+
+function sumar() {
+   var numeroInput = document.getElementById("nota").value;
+   var valor = numeroInput.valor;
+   alert("El valor ingresado es: "+valor)
+  }
